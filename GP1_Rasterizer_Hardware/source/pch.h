017699239a5891b7f5597b499a26e2979b02a4fc
SachaGDM <sacha.gonzalez.demuer@hotmail.com>
2022-12-12 20:13:56 +0100

    Software rasterizer initialization done
    Next step = fix resource leaks

--- GP1_Rasterizer_Hardware/source/pch.h
+++ GP1_Rasterizer_Hardware/source/pch.h
@@ -14,10 +14,10 @@
 #include "SDL_image.h"
 
 // DirectX Headers
-#include <dxgi.h>
-#include <d3d11.h>
-#include <d3dcompiler.h>
-#include <d3dx11effect.h>
+#include <dxgi.h> //dx11 api
+#include <d3d11.h> //dx11 shader compiler functionality
+#include <d3dcompiler.h> //dxgi api
+#include <d3dx11effect.h> //external effect framework
 
 // Framework Headers
 #include "Timer.h"
